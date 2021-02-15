bac18e59e30475234852c75b9162a86653c92d6b
pauloguerraf <paulo@nidointeractive.com>
2021-02-15 12:59:27 -0500

    updated work and experiments sections

--- components/TextLink.vue
+++ components/TextLink.vue
@@ -2,19 +2,31 @@
   <div>
     <nuxt-link v-if="internal" :to="url" class="py-4 text-center stroke-current">
         {{text}}
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline" viewBox="0 0 19.243 19.243">
+        <svg v-if="download" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline" viewBox="0 0 19.243 19.243">
           <g transform="translate(-8.379 -8.379)">
-            <path id="Path_2" data-name="Path 2" d="M10.5,25.5l15-15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-            <path id="Path_3" data-name="Path 3" d="M10.5,10.5h15v15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            <path d="M10.5,25.5l15-15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            <path d="M10.5,10.5h15v15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+          </g>
+        </svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline" viewBox="0 0 19.243 19.243">
+          <g transform="translate(-8.379 -8.379)">
+            <path d="M10.5,25.5l15-15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            <path d="M10.5,10.5h15v15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
           </g>
         </svg>
     </nuxt-link>
     <a v-else :href="url" class="bg-transparent py-4 text-center stroke-current">
         {{text}}
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline" viewBox="0 0 19.243 19.243">
+         <svg v-if="download" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline" viewBox="0 0 19.243 19.243">
           <g transform="translate(-8.379 -8.379)">
-            <path id="Path_2" data-name="Path 2" d="M10.5,25.5l15-15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-            <path id="Path_3" data-name="Path 3" d="M10.5,10.5h15v15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            <path d="M10.5,10.5l15,15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            <path d="M25.5,10.5v15h-15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+          </g>
+        </svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline" viewBox="0 0 19.243 19.243">
+          <g transform="translate(-8.379 -8.379)">
+            <path d="M10.5,25.5l15-15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            <path d="M10.5,10.5h15v15" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
           </g>
         </svg>
     </a>
@@ -35,6 +47,10 @@
       internal:{
         type: Boolean,
         default: true,
+      },
+      download:{
+        type: Boolean,
+        default: false,
       }
     }
   }
