b93d4ef8315017880898dc611d2fa41d64850142
pauloguerraf <paulo@nidointeractive.com>
2021-03-10 14:52:33 -0500

    3 to go

--- content/articles/saywhatisay_motomichi.md
+++ content/articles/saywhatisay_motomichi.md
@@ -8,4 +8,18 @@ alt: nice image
 
 ## Overview
 
-Visitors are welcomed to stand by the lectern placed in the center of the room and speak to the microphone. As you speak, or scream if you wish, the animated faces projected onto the four balloons react to your voice and move their mouths simultaneously with yours. Seeing not just one, but a group of faces in front of you following your every word may make you feel watched or uncomfortable or alternatively feel special or important, much like having your own entourage.
+**Say What I Say** is a project by projection artist <a href="https://www.motomichi.com/">Motomichi Nakamura</a>, in which visitors are welcomed to stand by a lectern placed in the center of a room and speak to a microphone. As visitors speak, (or scream if they wish), 4 animated faces projected onto balloons react to volume and frequency perceived from the voice and move their mouths simultaneously with the visitor. For this project I was contacted to remotely lead, assist and developed software for the installation. 
+
+<div class="imgs">
+<img src="/projects_images/saywhatisay/img_1.jpg">
+<img src="/projects_images/saywhatisay/img_2.jpg">
+</div>
+
+Time was short and budget limited, so we decided to use 4 **RaspberryPi** devices and **Openframeworks**. One RaspberryPi was responsible for reading audio input and send control messages to the other RaspberryPi. Each RaspberryPi would be running a custom built video mapping application that would react to the received messages resulting on the artist desired effect. 
+
+<div class="imgs">
+<img src="/projects_images/saywhatisay/img_3.jpg">
+<img src="/projects_images/saywhatisay/img_4.jpg">
+</div>
+
+I have kept helping Motomichi in some projects after collaborating with him on *Say What I Say*. 
--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -4,17 +4,19 @@
     <div id="title" class="mx-auto fixed font-bold text-center w-full h-screen text-myblue text-opacity-25">{{ article.title }}</div>
 
     <!--info-->
-    <div class="md:text-4xl text-3xl lg:px-40 md:px-36 px-20 text-myblue z-50 relative">
-      <div class="w-full h-full">
+    <div class="md:text-4xl text-3xl lg:px-40 md:px-36 px-20 text-myblue z-10 relative lg:m-10 m-8">
+      <TextLink url="/work" :back=true :internal=true text="back" class="font-bold hover:text-myyellow text-orange-500 text-2xl"/>
+      <div class="w-full h-full border-myblue border-l-4 pl-4">
         <div class="mt-16 -mb-3 flex uppercase text-sm">
           <span class="mr-3">•</span>
           <p>{{ article.description }}</p>
         </div>
-        <div class="text-6xl font-bold">{{ article.title }}</div>
+        <div class="text-6xl font-bold ">{{ article.title }}</div>
       </div>
       <div class="pt-6 text-xl">
        <nuxt-content :document="article" />
       </div>
+      <TextLink url="/work" :back=true :internal=true text="back" class="font-bold hover:text-myyellow text-orange-500 text-2xl"/>
     </div>
   </div>
 </template>
@@ -44,7 +46,7 @@ export default {
   font-size: 15vw !important;
 }
 .nuxt-content p {
-  margin-bottom: 20px;
+  margin-bottom: 40px;
 }
 .nuxt-content h2 {
   font-weight: bold;
@@ -62,11 +64,25 @@ export default {
 .nuxt-content img {
   width: 50%;
   padding: 8px;
+  border-radius: 15px;
 }
 .nuxt-content video {
   padding: 8px;
 }
 .nuxt-content .imgs{
+  width: 100%;
+  display: flex;
+  margin: 16px;
+  flex-wrap: wrap;
+}
+.nuxt-content .instagram{
+  width: 100%;
+  display: flex;
+  margin: 16px;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.nuxt-content iframe{
   width: 100%;
   display: flex;
   margin: 16px;
